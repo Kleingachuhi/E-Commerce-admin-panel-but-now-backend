1bb815ea2673c546537aac863c426b9c3780d72b
KellyBrianMurimi <kellybrianmurimi@gmail.com>
2025-06-25 12:19:57 +0300

    Validation update

--- server/utils/validators.py
+++ server/utils/validators.py
@@ -1,4 +1,9 @@
 def validate_category_input(data):
     errors = {}
     
-   
+    if not data.get('name'):
+        errors['name'] = 'Category name is required'
+    elif len(data['name']) < 2:
+        errors['name'] = 'Category name must be at least 2 characters'
+    
+    return errors
